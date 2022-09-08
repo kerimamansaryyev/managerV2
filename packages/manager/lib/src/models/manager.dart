ab8c1ef7e60da1ae5c563eb52a5e0b220ed52b46
Kerim Amansaryyev <keamansaryyev@gmail.com>
2022-09-08 14:15:53 +0500

    Added isDisposed

--- packages/manager/lib/src/models/manager.dart
+++ packages/manager/lib/src/models/manager.dart
@@ -11,6 +11,21 @@ import 'package:rxdart/rxdart.dart';
 typedef AsyncTaskCompleterReferenceTable<T>
     = Map<String, AsyncTaskCompleterReference<T>>;
 
+/// An event-driven structure that has the single [state] of a type [T].
+///
+/// State changes are encapsulated and only possible to be changed internally by calling
+/// [mutateState] or by running a [Task] via [run] method.
+///
+/// Key features and advantages:
+/// - Every single attempt to change the [state] has its id and will be tracked.
+/// - The manager allows running multiple [AsynchronousTask]s concurrently and track progress
+/// progress of them through [on] method.
+/// - It is possible to filter and listen to incoming [TaskEvent]s through [on] method by
+/// a type of a [Task] or by [Task.id].
+/// - If you run a [Task] and manager already has a task with the same id - the previous task will be dismissed.
+/// So you don't need to worry about unwanted outcomes.
+/// - An each [Task] that was completed with [TaskSuccessEvent] will change the state through [mutateState].
+/// - You can track an every state change by listening to [onStateChanged]
 abstract class Manager<T> {
   final AsyncTaskCompleterReferenceTable<T> _references = {};
   final StreamController<T> _onStateChangedController =
@@ -22,25 +37,37 @@ abstract class Manager<T> {
   late final BehaviorSubject<T> _onStateChangedControllerWithLatest;
 
   T _state;
+  bool _isDisposed = false;
 
   Manager(T initialValue)
       : _state = initialValue,
         _onStateChangedControllerWithLatest =
             BehaviorSubject.seeded(initialValue);
 
+  /// If [dispose] was called on the manager
+  bool get isDisposed => _isDisposed;
+
+  /// Current state of the manager.
   T get state => _state;
 
+  /// Emits an event each time [on] or [onStateChanged] fires an event.
   Stream<void> get onUpdated =>
       StreamGroup.mergeBroadcast([on(), onStateChanged()]).map((event) {
         return;
       });
 
+  /// Emits an event each time the [state] is changed in [mutateState]
+  ///
+  /// If [withLatest] is `true`, the stream will emit the latest immidiately event to every
+  /// subscriber that listens to it.
   Stream<T> onStateChanged({
     bool withLatest = false,
   }) =>
       _decideOnStateControllerStream(withLatest);
 
-  /// Filters according to the conditions:
+  /// Emits [TaskEvent] while tracking the [Task]s
+  ///
+  /// Emits an event if the condition is true:
   /// - `(taskId == null && S == Task<V>)` => both [taskId] and [S] are not provided
   ///
   /// or
@@ -53,7 +80,7 @@ abstract class Manager<T> {
   ///
   /// or
   ///
-  /// - `(taskId == null && task is S)` => [taskId] is not provided and [S] matches the bounds
+  /// - `(taskId == null && task is S)` => [taskId] is not provided and [S] matches the bounds.
   Stream<TaskEvent<T>> on<S extends Task<T>>({
     String? taskId,
     bool withLatest = false,
@@ -65,6 +92,12 @@ abstract class Manager<T> {
   }) =>
       _references[safelyExtractTaskIdFromString(taskId)];
 
+  /// * If the manager runs a [SynchronousTask] - only [TaskSuccessEvent] will be added to the [on] stream.
+  /// * If the manager runs an [AsynchronousTask] - the events will be added to the [on] stream depending on following cases:
+  ///   - It add [TaskLoadingEvent] to the [on] stream before running [AsynchronousTask.run]. If the manager is already tracking
+  ///   a task with the same [Task.id] and if the [task] is [CancelableAsyncTaskMixin] - a [task] will be killed before running. ([TaskKillEvent] `wont't` be emitted)
+  ///   - If future [AsynchronousTask.run] of [task] completes without error - [TaskSuccessEvent] will be added to the [on] stream and [mutateState] will be called and the [state] gets mutated. (If the reference to this task is not outdated.)
+  /// - If future [AsynchronousTask.run] of [task] completes with error - [TaskErrorEvent] will be added to the [on] stream. (If the reference to this task is not outdated.)
   @mustCallSuper
   void run(Task<T> task) {
     if (task is AsynchronousTask<T>) {
@@ -74,6 +107,11 @@ abstract class Manager<T> {
     }
   }
 
+  /// The method kills a task found by [taskId] and [TaskKillEvent] will be added to the [on] stream.
+  ///
+  /// It `won't` have effect if:
+  /// - If there are no references found for [taskId]. (Hadn't been run previously or already completed with either [TaskSuccessEvent] or [TaskErrorEvent])
+  /// - The referenced task is not [CancelableAsyncTaskMixin]
   @mustCallSuper
   Future<void> killById({required String taskId}) async {
     final reference = _references[safelyExtractTaskIdFromString(taskId)];
@@ -84,8 +122,12 @@ abstract class Manager<T> {
     return kill(task);
   }
 
+  /// Closing all the sinks of manager.
+  ///
+  /// `Warning`: if
   @mustCallSuper
   void dispose() {
+    _isDisposed = true;
     _onStateChangedController.close();
     _onStateChangedControllerWithLatest.close();
     _onEventController.close();
@@ -105,8 +147,7 @@ abstract class Manager<T> {
   @protected
   void mutateState(T newState) {
     _state = newState;
-    if (!_onStateChangedController.isClosed &&
-        !_onStateChangedControllerWithLatest.isClosed) {
+    if (!isDisposed) {
       _onStateChangedController.add(_state);
       _onStateChangedControllerWithLatest.add(state);
     }
@@ -178,8 +219,7 @@ abstract class Manager<T> {
   }
 
   void _passEvent(TaskEvent<T> event) {
-    if (!_onEventController.isClosed &&
-        !_onEventControllerWithLatestEvent.isClosed) {
+    if (!isDisposed) {
       _onEventController.add(event);
       _onEventControllerWithLatestEvent.add(event);
       onEventCallback(event);
@@ -240,7 +280,8 @@ abstract class Manager<T> {
     _changeState(task.run(), task);
   }
 
-  /// Filters according to the conditions:
+  /// Description:
+  ///
   /// - `(taskId == null && S == Task<V>)` => both [taskId] and [S] are not provided
   ///
   /// or
